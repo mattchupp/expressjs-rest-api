c881e596fb8563ae05819363df4f25fca03349b5
mattchupp <mattlchupp@gmail.com>
2020-05-23 10:31:56 -0500

    initial commit, set up basic static api for tickets

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 4000;
+
+
+let tickets = {
+  1: {
+    id: '1',
+    ticket_title: 'Test Ticket',
+    ticket_description: 'This is the first test ticket. I am having issues with this',
+    ticket_creator: 'Joe Creator',
+    ticket_owner: 'Matt C',
+    ticket_status: 'Open'
+  },
+  2: {
+    id: '2',
+    ticket_title: 'Bug Item 1',
+    ticket_description: 'Every time I create a new document it crashes',
+    ticket_creator: 'Mary Batman',
+    ticket_owner: 'Matt C',
+    ticket_status: 'Open'
+  }
+}
+
+
+
+
+app.get('/', (req, res) => res.send('This is the api!'))
+
+
+app.get('/api/tickets', (req, res) => {
+  return res.send(Object.values(tickets));
+})
+
+app.get('/api/tickets/:id', (req, res) => {
+  return res.send(tickets[req.params.id])
+})
+
+
+
+app.put('/', (req,res) =>
+  res.send('Got a PUT request at /user')
+)
+
+app.delete('/user', (req,res) =>
+  res.send('Got a DELETE request at /user')
+)
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
