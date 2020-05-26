0b3ccbd1b48f3dbeba63d4c8706142004320b7e5
mattchupp <mattlchupp@gmail.com>
2020-05-25 22:59:52 -0500

    add ticket route for api

---
+++ routes/tickets.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+
+// Get all Tickets
+
+// Get single ticket
+
+// Create new ticket
+
+// Updating a ticket
+
+// Deleting a ticket
+
+
+
+module.exports = router
